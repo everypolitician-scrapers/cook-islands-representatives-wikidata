3324afa250c86250246b515df13507559bf25d19
Tony Bowden <tony@tmtm.com>
2019-02-07 11:45:49 +0000

    Scrape term pages directly

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,22 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/cook-islands-elections-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+en12 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/12th_Cook_Islands_Parliament',
+  xpath: '//table[.//td[contains(.,"Electorate")]]//td[2]//a[not(@class="new")]/@title',
+  as_ids: true,
+)
+
+en13 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/13th_Cook_Islands_Parliament',
+  xpath: '//table[.//td[contains(.,"Electorate")]]//td[2]//a[not(@class="new")]/@title',
+  as_ids: true,
+)
+
+en14 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/14th_Cook_Islands_Parliament',
+  xpath: '//table[.//td[contains(.,"Electorate")]]//td[2]//a[not(@class="new")]/@title',
+  as_ids: true,
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: en12 | en13 | en14)
